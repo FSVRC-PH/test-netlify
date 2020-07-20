d3c1ddc27536f1b5962b148d4d43dae4f0c2ae8c
seanyu4296 <seanyu4296@gmail.com>
2020-07-20 02:43:10 +0000

    Update metadata description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `FSVRC`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `FIRST SHELTER VENTURES & REALTY CORP. is a real estate company based out of Philippines`,
+    author: `@Sean Yu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `gray`,
+        theme_color: `gray`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
